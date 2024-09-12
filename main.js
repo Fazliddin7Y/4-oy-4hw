62849e2ad77abf05e0bc0e6a2697f412f7161d96
Fazliddin7Y <ttfazliddin@gmail.com>
2024-09-12 23:34:10 +0500

    hww

---
+++ main.js
@@ -0,0 +1,33 @@
+console.log(parseInt('123')); // Natija: 123
+
+console.log(parseFloat('3.14')); // Natija: 3.14
+
+console.log(parseInt('0x1A')); // Natija: 26
+
+console.log(parseInt('1010', 2)); // Natija: 10
+
+console.log(Number.isInteger(4.5)); // Natija: false
+
+console.log(parseInt(' 345 ')); // Natija: 345
+
+console.log(parseInt('abc123')); // Natija: NaN
+
+console.log(parseInt('123', 8)); // Natija: 83
+
+console.log(Number.MIN_VALUE); // Natija: 5e-324
+
+console.log(parseInt('56.78')); // Natija: 56
+
+console.log(Math.round(7.5)); // Natija: 8
+
+console.log(parseFloat('9.81abc')); // Natija: 9.81
+
+console.log(isNaN('abc')); // Natija: true
+
+console.log(parseInt('0123')); // Natija: 123
+
+console.log(Number.MAX_SAFE_INTEGER); // Natija: 9007199254740991
+
+console.log((3.4567).toFixed(2)); // Natija: "3.46"
+
+console.log(Math.floor(78.99)); // Natija: 78
